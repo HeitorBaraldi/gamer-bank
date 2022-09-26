3a9fe944ef4b898f641e351c22e752921935b851
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-09-26 14:18:17 -0300

    ajustado rodape e home

--- src/components/Layouts/Rodape.jsx
+++ src/components/Layouts/Rodape.jsx
@@ -1,9 +1,23 @@
 import styles from './Rodape.module.css'
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
 
 const Rodape = () => {
     return (
             <footer className={styles.footer}>
+                <ul className={styles.social_list}>
+                    <li>
+                        <a href="https://www.facebook.com/"><FaFacebook/></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/sem/campaign/emailsignup/?campaign_id=13534421690&extra_1=s|c|547348930993|e|instagram%20%27|&placement=&creative=547348930993&keyword=instagram%20%27&partner_id=googlesem&extra_2=campaignid%3D13534421690%26adgroupid%3D126262409974%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-1321618851291%26loc_physical_ms%3D1001729%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIlcbC0fWy-gIVEemRCh3irgTfEAAYASAAEgKLxPD_BwE"><FaInstagram/></a>
+                    </li>
+                    <li>
+                        <a href="https://br.linkedin.com/?src=go-pa&trk=sem-ga_campid=12619604099_asid=122510712920_crid=606535073454_kw=linked_d=c_tid=kwd-103941963_n=g_mt=e_geo=1001729_slid=&mcid=6821526239111716925&cid=&gclid=EAIaIQobChMI6sbr6PWy-gIVc0FIAB2-fQ8gEAAYASAAEgL8tPD_BwE&gclsrc=aw.ds"><FaLinkedin/></a>
+                    </li>
+                </ul>
+                <p className={styles.copy_right}>contato@gamersbank.com</p>
                 <p className={styles.copy_right}><span>Gamer's Bank</span>&copy; 2022</p>
+                
             </footer>
     )
 }
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,18 +1,20 @@
 import styles from './Home.module.css'
 import {Link} from 'react-router-dom'
+import logoPs from '../../../img/psplus.png'
+import logoGp from '../../../img/gamepass.png'
+import logoSteam from '../../../img/steam.png'
+
 
 
 const Home =() => {
     return (
+        <main>
         <div className={styles.fundo}>
 
             <div className={styles.container}>
                 <h1>Bem vindo ao <span>Gamer's Bank</span></h1>
                 <p>Conheça o banco pensado para você: Gamer</p>
                 <p>Incriveis beneficios para não pausar seu jogo!!!</p>
-                {/*<Link to="/cadastro">
-                    <button>Cadastre agora</button>
-    </Link>*/}
                 
                 <div className={styles.div_btn_cad}>
                 <Link to ="/cadastro">
@@ -25,7 +27,28 @@ const Home =() => {
                 </div>
                 
             </div>
-         </div>
+            
+   
+        </div>
+
+        <div className={styles.fundo2}>
+            <div className={styles.container2e}>
+                <h1>Descontos incriveis</h1>
+                <p>Ganhe até 30% de desconto</p>
+                <p>utilizando nosso cartão de crédito</p>
+                <p>para pagamento das mensalidades no <span>Plano Anual</span></p>
+                <p>das principais plataformas de games !!!</p>
+            </div>
+            <div className={styles.container2d}>
+                <h1>Não pause seu jogo</h1>
+                <a href="https://store.playstation.com/pt-br/pages/latest"><img src={logoPs} alt="Ps" /></a>
+                <a href="https://www.xbox.com/pt-BR/xbox-game-pass/pc-game-pass?&ef_id=EAIaIQobChMI_oryouyy-gIVvEFIAB3m0geREAAYASAAEgJ-e_D_BwE:G:s&OCID=AIDcmmaigrfm8i_SEM_EAIaIQobChMI_oryouyy-gIVvEFIAB3m0geREAAYASAAEgJ-e_D_BwE:G:s&gclid=EAIaIQobChMI_oryouyy-gIVvEFIAB3m0geREAAYASAAEgJ-e_D_BwE"><img src={logoGp} alt ="Gp"/></a>
+                <a href="https://store.steampowered.com/?l=brazilian"><img src={logoSteam} alt="St" /></a>
+                
+            </div>
+        </div>
+        </main>
+
     )
 }
 
